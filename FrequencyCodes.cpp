c33e7e0a2b49b147043012454f44449ff2c3f1f1
tonyfarag2007 <tonywfarag2007@gmail.com>
2026-05-17 10:52:02 -0400

    Removed code generation test

--- FrequencyCodes.cpp
+++ FrequencyCodes.cpp
@@ -7,10 +7,6 @@
 void generateCodes(Node* rootNode, std::string currentCode, std::map <char, std::string> &currentCodes) {
     if (rootNode->left == nullptr && rootNode->right == nullptr) {
         currentCodes[rootNode->character] = currentCode;
-        auto key = currentCodes.find(rootNode->character);
-        if (key != currentCodes.end()) {
-            std::cout << key->first << ": " << key->second << std::endl;
-        }
         return;
     }
         generateCodes(rootNode->left, currentCode + "0", currentCodes);
